d59aa0ac974b94cbdf3db20f978b559ed2bf0b0d
Constantin Ziesche <constantin.ziesche@bosch.com>
2022-03-15 12:13:39 +0100

    Updated LICENSE

--- BaSyx.Registry.Client.Http/RegistryHttpClient.cs
+++ BaSyx.Registry.Client.Http/RegistryHttpClient.cs
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2020, 2021 Robert Bosch GmbH
+* Copyright (c) 2022 Bosch Rexroth AG
 * Author: Constantin Ziesche (constantin.ziesche@bosch.com)
 *
 * This program and the accompanying materials are made available under the
-* terms of the Eclipse Public License 2.0 which is available at
-* http://www.eclipse.org/legal/epl-2.0
+* terms of the MIT License which is available at
+* https://github.com/eclipse-basyx/basyx-dotnet/blob/main/LICENSE
 *
-* SPDX-License-Identifier: EPL-2.0
+* SPDX-License-Identifier: MIT
 *******************************************************************************/
 using BaSyx.API.Interfaces;
 using BaSyx.Models.Connectivity;

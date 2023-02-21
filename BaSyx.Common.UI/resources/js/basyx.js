d924a443dcd4cdc98ae489d069ef3aa623a9e807
Constantin Ziesche <constantin.ziesche@bosch.com>
2023-02-21 17:09:59 +0100

    Improved clear button in UI

--- BaSyx.Common.UI/resources/js/basyx.js
+++ BaSyx.Common.UI/resources/js/basyx.js
@@ -102,6 +102,7 @@ function GetPropertyValue(requestPath, hashedPathIdInput) {
 function ClearFields(hashedRequestPath) {
     $('.argInValue.' + hashedRequestPath).val('');
     $('.argOutValue.' + hashedRequestPath).val('');
+    $('#messageInput_' + hashedRequestPath).val('');
 }
 
 
